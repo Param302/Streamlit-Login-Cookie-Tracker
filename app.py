3d1d63bb49dba28bc1ead06195382d3c62e96f1e
Parampreet Singh <rubercuber.30@gmail.com>
2024-09-19 14:27:57 +0530

    Added Input Validation

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
+from math import e
+import re
 import json
 import datetime
 import firebase_admin
-from matplotlib.pylab import f
 import streamlit as st
 from streamlit_option_menu import option_menu
 from firebase_admin import credentials, firestore
@@ -20,11 +21,55 @@ db = firestore.client()
 
 st.set_page_config(page_title="Daily Kharcha")
 
-def valid_inputs(**kwargs):
-    print("Validating inputs")
-    for key, value in kwargs.items():
-        print(f"{key}: {value}")
-    return True
+def validate_inputs(**params) -> bool:  
+    empty_values = [f"**{key.title()}**" for key, value in params.items() if not value]
+    if empty_values:
+        print("Empty values", empty_values)
+        st.error(f"Please enter {(', '.join(empty_values[:-1]) + ' and ' + empty_values[-1]) if len(empty_values) > 1 else empty_values[0]}.")
+        return False
+    
+    errors = []
+    email = re.compile(r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$").match(params["email"].strip())  
+    if not email:
+        errors.append("Invalid email address.")
+    else:
+        email = email.group()
+    
+    # if sign in form
+    if len(params) == 2:
+        if errors:
+            st.error("Please enter valid email address and password.")
+            return False
+        return True
+
+    # if sign up form
+    password = params["password"].strip()
+    conf_password = params["confirm password"].strip()
+    name = params["name"].strip()
+
+    if len(password) < 8 or len(password) > 32:
+        errors.append("Password should be 8 to 32 characters long.")
+    if not re.search(r"[A-Z]", password):
+        errors.append("Password should contain at least one uppercase letter.")
+    if not re.search(r"[a-z]", password):
+        errors.append("Password should contain at least one lowercase letter.")
+    if not re.search(r"[0-9]", password):
+        errors.append("Password should contain at least one number.")
+    if not re.search(r"[^\w\d]", password):
+        errors.append("Password should contain at least one special character.")
+
+    if name.lower() in password.lower():
+        errors.append("Password should not contain your name.")
+
+    if password != conf_password.strip():
+        errors.append("Password and Confirm password do not match.")
+    
+    if errors:
+        error_msg = "**Please fix the following errors:**\n\n"
+        error_msg += "\n".join([f"- {error}" for error in errors])
+        st.error(error_msg)
+
+    return not errors
 
 
 def register_user(email, password, name):
@@ -101,12 +146,10 @@ else:
                 submit_button = st.form_submit_button("Register", type="primary", use_container_width=True)
             
             if submit_button:
-                if valid_inputs(**{"name": name, "email": email, "password": password, "conf_password": conf_password}):
+                if validate_inputs(**{"name": name, "email": email, "password": password, "confirm password": conf_password}):
                     print("REGISTERING")
                     print(email, password, name)
-                    # register_user(email, password, name)
-                else:
-                    error_space.error("Please enter valid details.")        
+                    # register_user(email, password, name)      
 
     elif nav_option == "Login":
         with st.form("login_form"):
@@ -124,11 +167,11 @@ else:
                 submit_button = st.form_submit_button("Login", type="primary", use_container_width=True)
             
             if submit_button:
-                if valid_inputs(**{"email": email, "password": password}):
+                if validate_inputs(**{"email": email, "password": password}):
                     print("LOGGING IN")
                     print(email, password)
                     # login_user(email, password)
-                else:
-                    st.error("Please enter email and password.")
+                # else:
+                #     st.error("Please enter email and password.")
     else:
         st.error("Invalid option selected.")
